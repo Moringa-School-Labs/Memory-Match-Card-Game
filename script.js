533a612b9972c89533864ec64607a075cab15f6f
catherine <vuthi.esther@gmail.com>
2022-10-04 22:28:25 +0300

    adds array

--- script.js
+++ script.js
@@ -11,4 +11,5 @@ function shuffleCards() {
   matchedPairs = 0; // reset matchedPairs variable to 0
   disableDeck = false; // reset disableDeck boolean to false
   cardOne = cardTwo = ""; // reset cardOne and cardTwo variables to empty string
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]; // create an array of the image numbers, 1-8, twice
 }
