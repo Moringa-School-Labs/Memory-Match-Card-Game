b216aef7bda17e9fe330a126b9545a7b47c0e63d
catherine <vuthi.esther@gmail.com>
2022-10-04 22:23:44 +0300

    creates flipCard function

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const cards = document.querySelectorAll(".card");
 let matchedPairs = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
+
+function flipCard() {
+  console.log("flipCard was executed");
+}
