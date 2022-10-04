1b1ac401f406c80109dca79ba3fa6ef7a22b4bcd
catherine <vuthi.esther@gmail.com>
2022-10-04 22:27:14 +0300

    adds local variable to shuffleCards function

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ let disableDeck = false;
 function flipCard() {
   console.log("flipCard was executed");
 }
+
+function shuffleCards() {
+  matchedPairs = 0; // reset matchedPairs variable to 0
+  disableDeck = false; // reset disableDeck boolean to false
+  cardOne = cardTwo = ""; // reset cardOne and cardTwo variables to empty string
+}
