741e56ed74b661860bdf3bf53aaa0a59c10f5008
catherine <vuthi.esther@gmail.com>
2022-10-11 21:37:25 +0300

    adss matchCard function

--- script.js
+++ script.js
@@ -4,27 +4,58 @@ let cardOne, cardTwo;
 let disableDeck = false;
 
 function flipCard(evt) {
-  console.log("flipCard was executed");
-  console.log(evt);
-  const clickedCard = evt.target;
+  const clickedCard = evt.target; 
   if (cardOne !== clickedCard && !disableDeck) {
-    // make sure that the current variable cardOne is not the same value as the clickedCard, AND that the deck is NOT disabled
-    clickedCard.classList.add("flip"); // add the 'flip' class to the classes currently assigned to the clickedCard
+   
+    clickedCard.classList.add("flip"); 
+    if (!cardOne) {
+      
+      return (cardOne = clickedCard); 
+    }
+   
+    cardTwo = clickedCard; 
+    disableDeck = true; 
+   
+    let cardOneImg = cardOne.querySelector(".back-view img").src; 
+    let cardTwoImg = cardTwo.querySelector(".back-view img").src; 
+    matchCards(cardOneImg, cardTwoImg);
   }
 }
 
+function matchCards(img1, img2) {
+  if (img1 === img2) {
+   
+    matchedPairs++; 
+    if (matchedPairs == 8) {
+      console.log("YOU WIN!");
+      return;
+    }
+    
+    cardOne.removeEventListener("click", flipCard); 
+    cardTwo.removeEventListener("click", flipCard); 
+    cardOne = cardTwo = ""; 
+    disableDeck = false;
+    return;
+  }
+ 
+  cardOne.classList.remove("flip");
+  cardTwo.classList.remove("flip");
+  cardOne = cardTwo = ""; 
+  disableDeck = false;
+  return;
+}
+
 function shuffleCards() {
-  matchedPairs = 0; // reset matchedPairs variable to 0
-  disableDeck = false; // reset disableDeck boolean to false
-  cardOne = cardTwo = ""; // reset cardOne and cardTwo variables to empty string
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]; // create an array of the image numbers, 1-8, twice
-  arr.sort(() => (Math.random() > 0.5 ? 1 : -1)); // randomly sort the array
+  matchedPairs = 0;
+  disableDeck = false;
+  cardOne = cardTwo = "";
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
   cards.forEach((card, i) => {
-    // loop over the set of cards. For each `card`...
-    card.classList.remove("flip"); // remove the 'flip' class
-    let imgTag = card.querySelector(".back-view img"); // find the back-view image tag by querying all the childNodes of the current card element for the '.back-view img' CSS selector
-    imgTag.src = `images/img-${arr[i]}.png`; // set the value of the src attribute on the current imgTag to a numbered filename based on our randomized array
-    card.addEventListener("click", flipCard); // add a click event listener to the current card to execute a function `flipCard` when clicked
+    card.classList.remove("flip");
+    let imgTag = card.querySelector(".back-view img");
+    imgTag.src = `images/img-${arr[i]}.png`;
+    card.addEventListener("click", flipCard);
   });
 }
 shuffleCards();
